fe22ecc49852ec2186d5e7ab2f59929fd3286cd5
AmericanoBoy <volgogradpoverka@yandex.ru>
2025-08-09 09:05:03 +0300

    Update increase_number_list_by_one.py

--- ALGORITMES_MODUL_5_HW_2/increase_number_list_by_one.py
+++ ALGORITMES_MODUL_5_HW_2/increase_number_list_by_one.py
@@ -9,16 +9,21 @@ def control_iteration_data(data):
     if not isinstance(data, int): raise TypeError('ЭЛЕМЕНТ ДОЛЖЕН БЫТЬ ЧИСЛОМ ТИПА: INT|FLOAT !')
 
 
+#1 вариант решения (в процессе итерации проверяем тип данных каждого элемента списка):
+
 def increase_number_list_by_one(array):
     '''
     функция, преобразующая входной список по принципу сложения 'числа' списка с числом один
     :param array: [1, 2, 7, 9]
     :return: [1, 2, 8, 0]
+    прим:
+    1.перед началом цикла проверяем входные данные на принадлежность входных данных к типу |list|
+    2.в теле цикла с помощью сторонней функции 'control_iteration_data' проверяем принадлежность каждого элемента к типу данных |int|
     '''
 
     control_input_data(array)
 
-    flag = False
+    flag = False #т.к. проверяем все элементы на тип данных- создаем переменную, которая будет 'фиксировать' наличие в списке цифры '9'
     for i in range(len(array)-1, -1, -1):
 
         control_iteration_data(array[i])
@@ -31,7 +36,28 @@ def increase_number_list_by_one(array):
 
     return [[1] + array, array][flag == True]
 
+#2 вариант решения (без проверки типа данных итерируемых элементов):
+
+def increase_number_list_by_one_without_control_iteration_element(array):
+    '''
+    функция, преобразующая входной список по принципу сложения 'числа' списка с числом один
+    :param array: [1, 2, 7, 9]
+    :return: [1, 2, 8, 0]
+    прим: перед началом цикла проверяем входные данные на принадлежность входных данных к типу |list|
+    '''
+
+    control_input_data(array)
+
+    for i in range(len(array)-1, -1, -1):
+        if array[i] != 9:
+            array[i] += 1
+            return array
+        array[i] = 0
+    return [1] + array
+        
+
 
 #array=[1,2,3,4,5,9,6,7,8,9,9]
 #array =[9,9,9]
+
 #print(increase_number_list_by_one(array))
